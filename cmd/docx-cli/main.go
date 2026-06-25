18cfb6726624fde11784bcd4822e544db70c713c
peerpackerhe <peerpackerhe@varadise.cloud>
2026-06-25 16:13:49 +0800

    update version

--- cmd/docx-cli/main.go
+++ cmd/docx-cli/main.go
@@ -39,7 +39,7 @@ type ReplacementRule struct {
 	New string `json:"new"`
 }
 
-var Version = "v0.2.3"
+var Version = "v0.2.4"
 
 // fileType returns the file type based on extension.
 func fileType(path string) string {
